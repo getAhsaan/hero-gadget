94a187a4887ec266b284ec7ead98b03e7754babe
getahsaan <getahsaan@gmail.com>
2023-04-09 01:15:40 +0600

    add route, header, home, about

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const myRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={myRouter} />
+);
