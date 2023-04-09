af5efe771a1a581c4ff648566b624505add8f0ae
getahsaan <getahsaan@gmail.com>
2023-04-09 17:48:37 +0600

    find product details by product id and using useState, useEffect

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { getStoredCart } from "../utils/fakeDB";
 import { useLoaderData } from "react-router-dom";
 
 const Cart = () => {
+  const [cart, setCart] = useState([]);
+
   const pData = useLoaderData();
-  const savedCart = getStoredCart();
-  let cart = [];
-  for (const id in savedCart) {
-    const foundCart = pData.find((product) => product.id === id);
-    if (foundCart) {
+  useEffect(() => {
+    const savedCart = getStoredCart();
+    let cartData = [];
+    for (const id in savedCart) {
+      const foundCart = pData.find((product) => product.id === id);
+      if (foundCart) {
         foundCart.quantity = savedCart[id];
-      cart.push(foundCart);
+        cartData.push(foundCart);
+      }
     }
-  }
+    setCart(cartData);
+  }, []);
+  console.log(cart);
+
   return <div>cart</div>;
 };
 
