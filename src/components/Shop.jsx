03d8ce3b9d124a6c1fa9be43b6498df71a9545a9
getahsaan <getahsaan@gmail.com>
2023-04-09 12:22:25 +0600

    set data to local storage

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import ProductCard from "./Cards/ProductCard";
+import { addToDB } from "../utils/fakeDB";
 
 const Shop = () => {
   const productData = useLoaderData();
+
+  // add to cart handler
+  const handleAddToCart = (id) => {
+    addToDB(id);
+  };
+
   return (
     <div className="product-container">
       {productData.map((product) => (
         <ProductCard
           key={product.id}
           product={product}
+          handleAddToCart={handleAddToCart}
         />
       ))}
     </div>
---
+++ src/utils/fakeDB.js
@@ -0,0 +1,24 @@
+// add data to local storage
+const addToDB = (id) => {
+  let shoppingCart = {};
+
+  // get previous data from local storage
+  const storedCart = localStorage.getItem("shopping-cart");
+  if (storedCart) {
+    shoppingCart = JSON.parse(storedCart);
+  }
+
+  // add quantity
+  const quantity = shoppingCart[id];
+  if (quantity) {
+    const newQuantity = quantity + 1;
+    shoppingCart[id] = newQuantity;
+  } else {
+    shoppingCart[id] = 1;
+  }
+
+  //   set data to local storage
+  localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
+};
+
+export { addToDB };
